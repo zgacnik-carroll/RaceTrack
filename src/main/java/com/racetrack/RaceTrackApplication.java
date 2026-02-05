f034f264e005118d1bf4414857bb39c5215cbd50
Zack Gacnik <zgacnik@carroll.edu>
2026-02-05 15:08:45 -0700

    Initial commit

---
+++ src/main/java/com/racetrack/RaceTrackApplication.java
@@ -0,0 +1,13 @@
+package com.racetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaceTrackApplication.class, args);
+    }
+
+}
