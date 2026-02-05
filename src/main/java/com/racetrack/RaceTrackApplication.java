b1fd76161ef4be25347fc6d63af109365411e9f3
Jace Claassen <jclaassen@carroll.edu>
2026-02-05 15:16:30 -0700

    Repo connection check

--- src/main/java/com/racetrack/RaceTrackApplication.java
+++ src/main/java/com/racetrack/RaceTrackApplication.java
@@ -3,6 +3,7 @@ package com.racetrack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*its jace testing for github connection*/
 @SpringBootApplication
 public class RaceTrackApplication {
 
